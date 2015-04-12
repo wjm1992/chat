79b4a2843b4615fcb2517a3241df3ae48ab872b3
wjm <wjm@MacBook-Pro-de-wjm-2.local>
2015-04-12 23:09:12 +0200

    first commit

---
+++ chat/chat.js
@@ -0,0 +1,235 @@
+var express = require("express");
+var fs = require("fs");
+var app = express();
+var port = 3700;
+
+var allUsers = [];
+var allGroups = {};
+var allFiles = {};
+var allClientSockets = {};
+var allSocketByUser = {};
+var adminByGroup = {};
+
+app.set('views',__dirname+'/tpl');
+app.set('view engine',"ejs");
+//app.engine('jade',require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req,res) {
+	//res.send("It works");
+	res.render("index.html");
+});
+
+function ensureExists(path,callback)
+{
+	var mask = 0777;
+	fs.mkdir(path,mask,function(err){
+		if(err)
+		{
+			if(err.code == 'EEXIST')
+				callback(null);
+			else
+				callback(err);
+		}
+		else
+			callback(err);
+	});
+}
+
+//app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket){
+	socket.emit('message',{message: 'welcome to the chat', username: 'System'});
+	socket.on('send',function(data){
+		sendData(data);
+		//io.sockets.emit('message',data);
+	});
+	
+	function sendData(data)
+	{
+		if(data.group == -1)
+		{
+			socket.emit('message',data);
+		}
+		else
+		{
+			var members = allGroups[data.group];
+			var len = members.length;
+			console.log("message in group: "+data.group+", members: "+members);
+			for(var i=0;i<len;i++)
+			{
+				var user = members[i];
+				//console.log("current user: "+user);
+				var socId = allSocketByUser[user];
+				var userSoc = allClientSockets[socId];
+				userSoc.emit('message',data);
+			}
+		}
+	}
+	
+	socket.on('login',function(data){
+		var idx = allUsers.indexOf(data.username);
+		if(idx == -1)
+		{
+			allUsers.push(data.username);
+			socket.emit('loginResult',{result:1});//userid not found, login successful
+			allClientSockets[socket.id] = socket;
+			allSocketByUser[data.username] = socket.id;
+		}
+		else
+			socket.emit('loginResult',{result:-1});//userid found, login failed
+	});
+	
+	socket.on('inviteUser',function(data){
+		var idx = allUsers.indexOf(data.username);
+		if(idx == -1)
+		{
+			socket.emit('alertError',{error:"User does not exist"});
+			return;
+		}
+		var socketId = allSocketByUser[data.username];
+		var socketInvited = allClientSockets[socketId];
+		
+		socketInvited.emit('invitationReceived',{username:data.host,group:data.group});
+	});
+	
+	socket.on('inviteAccept',function(data){
+		var hostId = allSocketByUser[data.host];
+		var socketHost = allClientSockets[hostId];
+		var groupId = -1;
+		var err = -1;
+		
+		if(data.accept == true)
+		{
+			var members = [];
+			if(data.group == -1)
+			{
+				groupId = data.host+Date.now();
+				
+				members[0] = data.host;
+				members[1] = data.invited;
+				
+				allGroups[groupId] = members;
+				adminByGroup[groupId] = data.host;
+				
+				console.log("new group created: "+groupId);
+			}
+			else
+			{
+				groupId = data.group;
+				members = allGroups[groupId];
+				if(members.length ==5)
+				{
+					err = "5 users per group maximal, cannot add more";
+				}
+				else
+				{
+					members.push(data.invited);
+				}
+			}
+			var len = members.length;
+			for(var i=0;i<len;i++)
+			{
+				var invitedId = allSocketByUser[members[i]];
+				var socketInvited = allClientSockets[invitedId];
+				socketInvited.emit('userInvited',{result:data.accept,admin:data.host,members:members,group:groupId,error:err});
+			}
+		}
+	});
+	
+	socket.on('banUser',function(data){
+		var admin = adminByGroup[data.group];
+		console.log("request user: "+data.requestUser+" ,admin: "+admin);
+		if(admin == -1 || admin != data.requestUser)
+			return;
+		
+		var members = allGroups[data.group];
+		var idx = members.indexOf(data.bannedUser);
+		
+		members.splice(idx,1);
+		
+		var len = members.length;
+		for(var i=0;i<len;i++)
+		{
+			var userId = allSocketByUser[members[i]];
+			var socketUser = allClientSockets[userId];
+			socketUser.emit('userBanned',{bannedUser:data.bannedUser,admin:admin,members:members,group:data.group});
+		}
+		
+		var userId = allSocketByUser[data.bannedUser];
+		var socketUser = allClientSockets[userId];
+		socketUser.emit('userBanned',{bannedUser:data.bannedUser,admin:admin,members:members,group:data.group});
+	});
+	
+	socket.on('startUpload',function(data){
+		var filename = data.filename;
+		var folderGroup = "/temp/"+data.group+"/";
+		var folderDate = Date.now()+"/";
+		var linkBase = folderGroup+folderDate;
+		folderGroup = "public"+folderGroup;
+		allFiles[filename] = {size:data.size,data:"",downloaded:0,handler:0,filetype:data.filetype,folder:"public/"+linkBase,link:linkBase+filename};
+		var folder = allFiles[filename].folder;
+		ensureExists(folderGroup,function(err){
+			ensureExists(folderGroup+folderDate,function(err){
+				var place = 0;
+				try
+				{
+					var stat = fs.statSync(folder+filename);
+					if(stat.isFile())
+					{
+						allFiles[filename].downloaded = stat.size;
+						place = stat.size / 524288;
+					}
+				}
+				catch(err){}
+				fs.open(folder+filename,"a",0755,function(err,fd){
+					if(err)
+					{
+						console.log(err);
+					}
+					else
+					{
+						allFiles[filename].handler = fd;
+						socket.emit('moreData',{place:place,percent:0});
+					}
+				});
+			});
+		});
+	});
+	
+	socket.on('proceedUpload',function(data){
+		var filename = data.filename;
+		var file = allFiles[filename];
+		file.downloaded += data.data.length;
+		file.data += data.data;
+		if(file.downloaded == file.size)//download fini
+		{
+			fs.write(file.handler,file.data,null,'Binary',function(err,writen){
+				//get thunbnail here
+				socket.emit('finishUpload',{filename:filename});
+				var msg = {username:data.username,group:data.group,video:true,link:file.link,filename:filename,filetype:file.filetype};
+				sendData(msg);
+			});
+		}
+		else
+		{
+			if(file.data.length > 10485760)//data buffer reaches 10MB
+			{
+				fs.write(file.handler,file.data,null,'Binary',function(err,written){
+					file.data = "";//reset buffer
+					var place = file.downloaded / 524288;
+					var percent = file.downloaded/file.size *100;
+					socket.emit('moreData',{place:place,percent:percent});
+				});
+			}
+			else
+			{
+				var place = file.downloaded/524288;
+				var percent = file.downloaded/file.size*100;
+				socket.emit('moreData',{place:place,percent:percent});
+			}
+		}
+	});
+});
+console.log("Listening on port "+port);
---
+++ chat/public/client.js
@@ -0,0 +1,363 @@
+var messages = [];
+var socket;
+var field;
+var sendButton ;
+var content;
+var name;
+var loginButton;
+var groupId = -1;
+var username
+var sendMediaButton;
+
+function getExtension(filename)
+{
+	var parts = filename.split(".");
+	return parts[parts.length-1];
+}
+
+function isImage(file)
+{
+	var ext = getExtension(file.name);
+	switch(ext.toLowerCase())
+	{
+	case 'jpg':
+	case 'jpeg':
+	case 'bmp':
+	case 'png':
+	case 'gif':
+		return true;
+	}
+	
+	return false;
+}
+
+function isVideo(file)
+{
+	var ext = getExtension(file.name);
+	switch(ext.toLowerCase())
+	{
+	case 'mov':
+	case 'mp4':
+	case 'avi':
+	case 'mpg':
+	case 'm4v':
+		return true;
+	}
+	
+	return false;
+}
+
+var sitePath = "http://localhost:3700";
+
+window.onload = function() {
+	socket = io.connect(sitePath);
+	field = document.getElementById("field");
+	sendButton = document.getElementById("send");
+	content = document.getElementById("content");
+	name = document.getElementById("name");
+	loginButton = document.getElementById("login");
+	sendMediaButton = document.getElementById("sendMedia");
+	
+	socket.on('alertError',function(data){
+		alert(data.error);
+	});
+	
+	socket.on('message', function(data){
+		//if(data.message)
+		//{
+			messages.push(data);
+			var html = '';
+			var current;
+			for(var i=0;i<messages.length;i++)
+			{
+				current = messages[i];
+				html += '<b>' + (current.username ? current.username : 'Server') + ': </b>';
+				if(current.image == true)
+				{
+					html += "<img class=\"msgImage\" src=\'"+current.buffer+"\'/><br/>"
+				}
+				else
+				{
+					if(current.video == true)
+					{
+						//html += "<a href=\""+sitePath+current.link+"\">"+current.filename+"</a><br/>";
+						html += "<video class=\'msgVideo\' width=\"400px\" controls><source src=\""+sitePath+current.link+"\" type=\""+current.filetype+"\"></video><br/>";
+					}
+					else
+						html += current.message+'<br/>';
+				}
+				
+			}
+			content.innerHTML = html;
+			content.scrollTop = content.scrollHeight;
+			/*}
+			else
+			{
+			console.log("There is a problem: ",data);
+			}*/
+	});
+	
+	sendButton.onclick = function() {
+		if(username == "")
+		{
+			alert("Please type your name!");
+		}
+		else
+		{
+			var text = field.value;
+			socket.emit('send',{message: text, username: username,group:groupId});
+			field.value = "";
+		}
+	};
+	
+	loginButton.onclick = function loginClicked()
+	{
+	
+		var nameField = document.getElementById("name");
+		if(nameField.value == "")
+			return;
+	
+		username = nameField.value;
+		socket.emit('login',{username:username});
+	};
+	
+	socket.on('loginResult',function(data){
+		displayLogin(data.result);
+	});
+	
+	function addUserInGroup(nameVal)
+	{
+		var node = document.createElement("div");
+		var text = document.createTextNode(nameVal);
+		node.appendChild(text);
+		
+		var memberList = document.getElementById("members");
+		
+		node.className = "chatMember";
+		memberList.appendChild(node);
+	}
+	
+	function displayGroupMembers(members,admin)
+	{
+		var len = members.length;
+		var html = "";
+		
+		for(var i=0;i<len;i++)
+		{
+			html+="<div class=\"chatMember\">"+members[i];
+			
+			if(members[i]==admin)
+			{
+				html+="(admin)";
+				if(members[i] == username)
+				{
+					html+="(self)";
+				}
+			}
+			else
+			{
+				if(members[i] == username)
+				{
+					html+="(self)";
+				}
+				else
+				{
+					html += "<input type=\"button\" class=\"banButton\" value=\"ban\" onclick=\"banUserInGroup(\'"+members[i]+"\');\"/>";
+				}
+			}
+			
+			html += "</div>";
+		}
+		
+		var buttons = document.getElementsByClassName("banButton");
+		var len = buttons.length;
+		if(admin == username)
+		{
+			for(var i=0;i<len;i++)
+			{
+				buttons[i].disabled = false;
+				//buttons[i].addEventListener("click",function(){
+				//	banUserInGroup(members[i]);
+				//});
+			}
+		}
+		else
+		{
+			for(var i=0;i<len;i++)
+			{
+				buttons[i].disabled = true;
+			}
+		}
+		
+		var memberList = document.getElementById("members");
+		memberList.innerHTML = html;
+	}
+
+	function displayLogin(success)
+	{
+		if(success == 1)
+		{
+			var loginCover = document.getElementById("cover");
+			var loginFrame = document.getElementById("loginDiv");
+	
+			loginCover.style.display = "none";
+			loginFrame.style.display = "none";
+			
+			addUserInGroup(username);
+		}
+		else
+		{
+			var loginResult = document.getElementById("loginResult");
+			loginResult.innerHTML = "Username duplicated, choose another please."
+		}
+	}
+	
+	var inviteButton = document.getElementById("invite");
+	inviteButton.onclick = function()
+	{
+		var nameField = document.getElementById("searchName");
+		if(nameField.value == "")
+			return;
+		else
+			socket.emit("inviteUser",{username:nameField.value,host:username,group:groupId});
+	};
+	
+	socket.on('userInvited',function(data){
+		if(data.err == -1)
+		{
+			alert(err);
+			return;
+		}
+		if(data.result == true)
+		{
+			displayGroupMembers(data.members,data.admin);
+			//addUserInGroup(data.username);
+			groupId = data.group;
+			
+			if(data.admin == username)
+				inviteButton.disabled = false;
+			else
+				inviteButton.disabled = true;
+		}
+		else
+		{
+			var inviteResult = document.getElementById("inviteResult");
+			inviteResult.innerHTML = "User not exists or declined invitation";
+		}
+	});
+	
+	socket.on('invitationReceived',function(data){
+		var result = confirm("You are invited by user: "+data.username+", join?");
+		socket.emit('inviteAccept',{accept:result,host:data.username,invited:username,group:data.group});
+	});
+	
+	socket.on('userBanned',function(data){
+		if(data.bannedUser == username)
+		{
+			alert("You are banned by "+data.admin+" from group, bye!");
+			var memberList = document.getElementById("members");
+			memberList.innerHTML = "<div class=\"chatMember\">"+username+"</div>";
+			inviteButton.disabled = false;
+		}
+		else
+		{
+			displayGroupMembers(data.members,data.admin);
+		}
+	});
+	
+	var files;
+	function handleFileSelect(evt) {
+	    evt.stopPropagation();
+	    evt.preventDefault();
+
+	    files = evt.dataTransfer.files; // FileList object.
+		
+		file = files[0];
+		if(!isImage(file)&&!isVideo(file))
+		{
+			alert("Sorry, not supported format.");
+			return;
+		}
+
+	    // files is a FileList of File objects. List some properties.
+	    var output = [];
+	    for (var i = 0, f; f = files[i]; i++) {
+	      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+	                  f.size, ' bytes, last modified: ',
+	                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+	                  '</li>');
+	    }
+	    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+	  }
+
+	  function handleDragOver(evt) {
+	    evt.stopPropagation();
+	    evt.preventDefault();
+	    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+	  }
+
+	  // Setup the dnd listeners.
+	  var dropZone = document.getElementById('dropZone');
+	  dropZone.addEventListener('dragover', handleDragOver, false);
+	  dropZone.addEventListener('drop', handleFileSelect, false);
+	  
+	  var reader;
+	  var file;
+  	sendMediaButton.onclick = function(){
+  		reader = new FileReader();
+		
+		file = files[0];
+		if(isImage(file))
+		{
+			reader.onload = function(){
+				socket.emit('send',{image:true,buffer:reader.result,username:username,group:groupId});
+				uploadFinished();
+			};
+			reader.readAsDataURL(file);
+		}
+		else
+		{
+			reader.onload=function(){
+				socket.emit('proceedUpload',{video:true,filename:file.name,size:file.size,filetype:file.type,username:username,group:groupId,data:reader.result});
+			};
+			
+			socket.emit('startUpload',{video:true,filename:file.name,size:file.size,filetype:file.type,username:username,group:groupId});
+			sendMediaButton.disabled = true;
+		}
+  	};
+	var progressBar = document.getElementById("progress");
+	
+	socket.on('moreData',function(data){
+		var place = data.place * 524288;
+		var newFile;
+		progressBar.innerHTML = data.percent +"%";
+		
+		if(file.slice)
+		{
+			newFile = file.slice(place,place+Math.min(524288,(file.size-place)));
+		}
+		else
+		{
+			newFile = file.mozSlice(place,place+Math.min(524288,(file.size-place)));
+		}
+		reader.readAsBinaryString(newFile);
+	});
+	
+	function uploadFinished()
+	{
+		progressBar.innerHTML = "100%";
+		sendMediaButton.disabled = false;
+		document.getElementById("list").innerHTML = "";
+		dropZone.innerHTML = "Drop image/video here";
+	}
+	
+	socket.on('finishUpload',function(data){
+		uploadFinished();
+	});
+}
+
+function banUserInGroup(name)
+{
+	socket.emit('banUser',{group:groupId,bannedUser:name,requestUser:username});
+}
+
